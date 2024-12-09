f3fb012bc30e76a12e48ea9a017b9e09b3391572
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2024-12-09 22:59:17 +0200

    improve examples

--- README.md
+++ README.md
@@ -23,7 +23,22 @@ pip install tauripy
 
 ## Examples
 
-See [examples](examples)
+```python
+from tauripy import Tauri
+from pathlib import Path
+import json
+
+def on_command(message: bytes):
+    print(f"Received: {message.decode('utf-8')}")
+    return json.dumps({'message': 'Hello from Python!'}).encode('utf-8')
+
+tauric = Tauri("com.tauric.dev", "tauric")
+tauric.mount_frontend('./dist')
+tauric.on_command(on_command)
+tauric.run(lambda: tauric.create_window("example_window", "tauric", "fs://index.html"))
+```
+
+See also [examples](examples)
 
 ## Roadmap
 
--- examples/main.py
+++ examples/main.py
@@ -10,16 +10,7 @@ def on_command(message: bytes):
     print(f"Received: {message.decode('utf-8')}")
     return json.dumps({'message': 'Hello from Python!'}).encode('utf-8')
 
-def on_ready(tauric):
-    tauric.create_window("example_window", "tauric", "fs://index.html")
-
-def main() -> None:
-    tauric = Tauri("com.tauric.dev", "tauric")
-    current_path = Path(__file__).parent
-    dist_path = current_path / 'dist'
-    tauric.mount_frontend(str(dist_path.absolute()))
-    tauric.on_command(on_command)
-    tauric.run(lambda: on_ready(tauric))
-
-if __name__ == "__main__":
-    main()
+tauric = Tauri("com.tauric.dev", "tauric")
+tauric.mount_frontend('./dist')
+tauric.on_command(on_command)
+tauric.run(lambda: tauric.create_window("example_window", "tauric", "fs://index.html"))
